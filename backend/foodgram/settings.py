ef8df6377c1e8b5c1aa9379f20a5f9a929ac8e8f
Иван <dert.0.1@yandex.ru>
2022-02-21 17:57:46 +0700

    fix database

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -81,30 +81,30 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 ENV = os.getenv('ENV')
 
-if ENV == 'DEV':
+# if ENV == 'DEV':
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# else:
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv(
+            'DB_ENGINE',
+            default='django.db.backends.postgresql'
+        ),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
-
-else:
-
-    DATABASES = {
-        'default': {
-            'ENGINE': os.getenv(
-                'DB_ENGINE',
-                default='django.db.backends.postgresql'
-            ),
-            'NAME': os.getenv('DB_NAME', default='postgres'),
-            'USER': os.getenv('POSTGRES_USER', default='postgres'),
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-            'HOST': os.getenv('DB_HOST', default='db'),
-            'PORT': os.getenv('DB_PORT', default='5432')
-        }
-    }
+}
 
 dcap = 'django.contrib.auth.password_validation.'
 AUTH_PASSWORD_VALIDATORS = [
